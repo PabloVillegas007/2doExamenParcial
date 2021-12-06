7679c16685c4e865799e4dfbbdcef0c71026b0c1
PabloVillegas007 <91212674+PabloVillegas007@users.noreply.github.com>
2021-12-06 05:41:02 -0400

    Add files via upload

---
+++ Pregunta5 ASP.NET/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Pregunta5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Serie Fibonacci"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server" Width="517px"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Generar" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Limpiar" />
+    </form>
+</body>
+</html>
---
+++ Pregunta5 ASP.NET/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Pregunta5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int a, b, limite, i, auxiliar;
+            limite = Int32.Parse(TextBox1.Text);
+            a = 0;
+            b = 1;
+            for (i = 0; i < limite; i++)
+            {
+                auxiliar = a;
+                a = b;
+                b = auxiliar + a;
+                TextBox2.Text = TextBox2.Text.ToString() + a + ", ";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+        }
+    }
+}
